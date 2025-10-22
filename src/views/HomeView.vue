2735db5714b38979483d9cbbd4fa8d28445ae998
raphaellyzzz <devarirapha@gmail.com>
2025-10-22 16:10:43 -0300

    finalizando issue - banner sumir quando digitar Fixes#23

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="home-page">
-    <Banner />
+    <Banner v-if="!searchText" />
+
+    <h2 v-if="searchText" class="results-title">
+      Resultados para "{{ searchText }}"
+    </h2>
+
     <GameList />
   </div>
 </template>
 
 <script>
+import { mapState } from 'pinia';
+import { useSearchStore } from '../stores/searchStore';
+
 import Banner from '../components/home/Banner.vue';
 import GameList from '../components/home/GameList.vue';
 
@@ -14,6 +22,19 @@ export default {
   components: {
     Banner,
     GameList
+  },
+  computed: {
+    ...mapState(useSearchStore, ['searchText']),
   }
 };
 </script>
+
+<style scoped>
+.results-title {
+  color: white;
+  text-align: center;
+  font-size: 1.5rem;
+  margin-bottom: 20px;
+  padding: 0 50px;
+}
+</style>
